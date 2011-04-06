6e569644443b1dd83b0c8de4716b0d26d9a5890f
Linus Ericsson <oscarlinusericsson@gmail.com>
2011-04-06 20:00:02 +0200

    den bästa blandningen av bibliotek

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sclougiv "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [clj-webdriver "0.2.1"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+		     [midje "1.1"]])
+
---
+++ src/sclougiv/core.clj
@@ -0,0 +1,20 @@
+(ns sclougiv.core
+  (:use midje.sweet)
+  (:use clj-webdriver.core))
+
+;;midje
+
+
+  
+  (def b (start :firefox "https://github.com"))
+
+  (-> b
+    (find-it {:text "Login"})
+    click)
+
+  (fact (let [testvardet (-> b
+			   (find-it {:xpath "//a[text()='Blog']"}))]
+	  (count (list testvardet)) => 1
+	(count (list testvardet)) => 2))
+
+;;(count (list (-> b (find-it {:xpath "//a[text()='Blog']"}))))
